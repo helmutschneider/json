8b2476844702f5f92a0a32bdee5cad44a760f696
Johan Björk <johanimon@gmail.com>
2021-09-16 21:03:58 +0200

    OCaml: fancy function for parsing strings

--- ocaml/lib/lib.ml
+++ ocaml/lib/lib.ml
@@ -1,2 +1,28 @@
+type node =
+  | String of string
+  | Number of float
+  | Boolean of bool
+  | Null
+
+type parser = {
+  buffer: char list;
+  index: int;
+}
+
+exception ParserError of string
+
+let read_string str =
+  let rec read_from chunk carry =
+    match chunk with
+    | ['"'] -> carry
+    | '"'::tail -> read_from tail carry
+    | '\\'::x::tail -> read_from tail (List.append carry [x])
+    | head::tail -> read_from tail (List.append carry [head])
+    | _ -> raise (ParserError("Bruh!"))
+  in
+  read_from str [] |>
+    List.to_seq |>
+    String.of_seq
+
 let square : (float -> float)
   = fun x -> x *. x
--- ocaml/test/test.ml
+++ ocaml/test/test.ml
@@ -1,12 +1,27 @@
 open OUnit2
 open Lib
 
+let print s = s
+
 let test_square _ =
   assert_equal 25.0 (square 5.0)
+
+let test_read_string _ =
+  let str = read_string ['"'; 'H'; 'e'; 'l'; 'l'; 'o'; '"'] in
+  assert_equal "Hello" str
+
+let test_read_escaped_char _ =
+  let str = read_string ['"'; '\\'; '"'; '"'] in
+
+  print_endline str;
+
+  assert_equal "\"" str
 
 let suite =
   "Unit tests" >::: [
     "test_square" >:: test_square;
+    "test_read_string" >:: test_read_string;
+    "test_read_escaped_char" >:: test_read_escaped_char;
   ]
 
 let () =
