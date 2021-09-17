77ce91155c7db1dcfd23375d370050a49110af1c
Johan Björk <johanimon@gmail.com>
2021-09-17 02:25:22 +0200

    OCaml: add the rest of the parser

--- ocaml/lib/lib.ml
+++ ocaml/lib/lib.ml
@@ -2,27 +2,130 @@ type node =
   | String of string
   | Number of float
   | Boolean of bool
+  | Array of node list
+  | Object of (string, node) Hashtbl.t
   | Null
 
-type parser = {
-  buffer: char list;
-  index: int;
+type parse_output = {
+  node : node;
+  remainder : char list;
 }
 
-exception ParserError of string
+type parse_fn = char list -> parse_output
 
-let read_string str =
-  let rec read_from chunk carry =
-    match chunk with
-    | ['"'] -> carry
-    | '"'::tail -> read_from tail carry
-    | '\\'::x::tail -> read_from tail (List.append carry [x])
-    | head::tail -> read_from tail (List.append carry [head])
-    | _ -> raise (ParserError("Bruh!"))
+exception ParseError of string
+
+let rec skip_whitespace : char list -> char list =
+ fun chars ->
+  match chars with
+  | ' ' :: tail -> skip_whitespace tail
+  | '\n' :: tail -> skip_whitespace tail
+  | '\r' :: tail -> skip_whitespace tail
+  | '\t' :: tail -> skip_whitespace tail
+  | _ -> chars
+
+let read_string : parse_fn =
+ fun chars ->
+  let rec aux carry remainder =
+    match remainder with
+    (* escaped characters *)
+    | '\\' :: x :: tail -> aux (List.append carry [ x ]) tail
+    (* start of string *)
+    | '"' :: tail when carry = [] -> aux carry tail
+    (* end of string *)
+    | '"' :: tail when carry != [] -> (carry, tail)
+    (* regular characters *)
+    | x :: tail -> aux (List.append carry [ x ]) tail
+    | _ -> raise (ParseError "Supa bad!!")
   in
-  read_from str [] |>
-    List.to_seq |>
-    String.of_seq
 
-let square : (float -> float)
-  = fun x -> x *. x
+  let carry, remainder = aux [] chars in
+  let str = List.to_seq carry |> String.of_seq in
+
+  { node = String str; remainder }
+
+let is_number_like ch =
+  ch = '0' || ch = '1' || ch = '2' || ch = '3' || ch = '4' || ch = '5'
+  || ch = '6' || ch = '7' || ch = '8' || ch = '9' || ch = '.'
+
+let read_number : parse_fn =
+ fun chars ->
+  let rec aux carry remainder =
+    match remainder with
+    | head :: tail when is_number_like head ->
+        aux (List.append carry [ head ]) tail
+    | _ -> (carry, remainder)
+  in
+  let number_chars, rem = aux [] chars in
+  let number_str = List.to_seq number_chars |> String.of_seq in
+
+  { node = Number (float_of_string number_str); remainder = rem }
+
+let rec read_node : parse_fn =
+ fun chars ->
+  let chars = skip_whitespace chars in
+  match chars with
+  | '"' :: _ -> read_string chars
+  | '[' :: _ -> read_array chars
+  | '{' :: _ -> read_object chars
+  | 't' :: 'r' :: 'u' :: 'e' :: tail ->
+      { node = Boolean true; remainder = tail }
+  | 'f' :: 'a' :: 'l' :: 's' :: 'e' :: tail ->
+      { node = Boolean false; remainder = tail }
+  | 'n' :: 'u' :: 'l' :: 'l' :: tail -> { node = Null; remainder = tail }
+  | head :: _ when is_number_like head -> read_number chars
+  | _ -> raise (ParseError "Very bad!")
+
+and read_array : parse_fn =
+ fun chars ->
+  let rec aux carry remainder =
+    let rem = skip_whitespace remainder in
+    match rem with
+    | '[' :: tail -> aux carry tail
+    | ']' :: tail -> (carry, tail)
+    | ',' :: tail -> aux carry tail
+    | _ ->
+        let res = read_node rem in
+        let nodes = List.append carry [ res.node ] in
+        aux nodes res.remainder
+  in
+  let nodes, remainder = aux [] chars in
+  { node = Array nodes; remainder }
+
+and read_object : parse_fn =
+ fun chars ->
+  let tbl = Hashtbl.create 16 in
+  let rec aux remainder =
+    let rem = skip_whitespace remainder in
+    match rem with
+    | '{' :: tail -> aux tail
+    | '}' :: tail -> tail
+    | ',' :: tail -> aux tail
+    | '"' :: _ -> (
+        let key_node = read_string rem in
+        let key_str =
+          match key_node.node with
+          | String x -> x
+          | _ -> raise (ParseError "Invalid key type.")
+        in
+        match skip_whitespace key_node.remainder with
+        | ':' :: tail ->
+            let value_node = read_node tail in
+            Hashtbl.add tbl key_str value_node.node;
+            aux value_node.remainder
+        | _ -> rem)
+    | _ ->
+        let str = String.of_seq (List.to_seq rem) in
+        print_endline str;
+        raise (ParseError "Bad bad!")
+  in
+
+  let rem = aux chars in
+
+  { node = Object tbl; remainder = rem }
+
+let parse : string -> node =
+ fun str ->
+  let chars = String.to_seq str |> List.of_seq in
+  let out = read_node chars in
+  out.node
--- ocaml/test/test.ml
+++ ocaml/test/test.ml
@@ -1,29 +1,103 @@
 open OUnit2
 open Lib
 
-let print s = s
-
-let test_square _ =
-  assert_equal 25.0 (square 5.0)
+let test_skip_whitespace _ =
+  let bruh = [ ' '; '\n'; 'A'; 'B' ] in
+  assert_equal [ 'A'; 'B' ] (skip_whitespace bruh)
 
 let test_read_string _ =
-  let str = read_string ['"'; 'H'; 'e'; 'l'; 'l'; 'o'; '"'] in
-  assert_equal "Hello" str
+  let node = parse "\"Hello\"" in
+  assert_equal (String "Hello") node
 
 let test_read_escaped_char _ =
-  let str = read_string ['"'; '\\'; '"'; '"'] in
+  let node = parse "\"\\\"\"" in
+  assert_equal (String "\"") node
 
-  print_endline str;
+let test_read_array _ =
+  let node = parse "[\"yee\", \"boi\"]" in
+  assert_equal (Array [ String "yee"; String "boi" ]) node
 
-  assert_equal "\"" str
+let test_read_array_with_whitespace _ =
+  let node = parse "[           \"yee\",   \"boi\" \n \n \t]" in
+  assert_equal (Array [ String "yee"; String "boi" ]) node
+
+let test_read_bool _ =
+  let a = parse "true" in
+  let b = parse "false" in
+  assert_equal (Boolean true) a;
+  assert_equal (Boolean false) b
+
+let test_read_null _ =
+  let a = parse "null" in
+  assert_equal Null a
+
+let test_read_object_with_string_value _ =
+  let tbl = Hashtbl.create 16 in
+  Hashtbl.add tbl "yee" (String "boi");
+  let a = parse "{ \"yee\" : \"boi\" }" in
+  assert_equal (Object tbl) a
+
+let test_read_object_with_nested_stuff _ =
+  let tbl = Hashtbl.create 16 in
+  Hashtbl.add tbl "yee" (Array [ String "boi" ]);
+  let a = parse "{ \"yee\" : [\"boi\"] }" in
+  assert_equal (Object tbl) a
+
+let test_read_number _ =
+  let a = parse "123.5" in
+  assert_equal (Number 123.5) a
+
+let test_big_thing _ =
+  let yee =
+    {big_thing|
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "isAlive": true,
+    "age": 27,
+    "address": {
+      "streetAddress": "21 2nd Street",
+      "city": "New York",
+      "state": "NY",
+      "postalCode": "10021-3100"
+    },
+    "phoneNumbers": [
+      {
+        "type": "home",
+        "number": "212 555-1234"
+      },
+      {
+        "type": "office",
+        "number": "646 555-4567"
+      }
+    ],
+    "children": [],
+    "spouse": null
+}
+  |big_thing}
+  in
+  let res = parse yee in
+  assert_bool "Bad!"
+    (match res with
+    | Object _ -> true
+    | _ -> false)
 
 let suite =
-  "Unit tests" >::: [
-    "test_square" >:: test_square;
-    "test_read_string" >:: test_read_string;
-    "test_read_escaped_char" >:: test_read_escaped_char;
-  ]
+  "Unit tests"
+  >::: [
+         "test_skip_whitespace" >:: test_skip_whitespace;
+         "test_read_string" >:: test_read_string;
+         "test_read_escaped_char" >:: test_read_escaped_char;
+         "test_read_array" >:: test_read_array;
+         "test_read_array_with_whitespace" >:: test_read_array_with_whitespace;
+         "test_read_bool" >:: test_read_bool;
+         "test_read_null" >:: test_read_null;
+         "test_read_object_with_single_key"
+         >:: test_read_object_with_string_value;
+         "test_read_object_with_nested_stuff"
+         >:: test_read_object_with_nested_stuff;
+         "test_read_number" >:: test_read_number;
+         "test_big_thing" >:: test_big_thing;
+       ]
 
-let () =
-  run_test_tt_main suite
-  
+let () = run_test_tt_main suite
